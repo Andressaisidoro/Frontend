f1debd71820f7368eca3b0d17cf880293937ae47
Andressa Isidoro <dessa_silva93@outlook.com>
2024-05-28 20:42:49 -0300

    2805

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
+
+  const decrementar = () => {
+    setContador(contador - 1);
+  };
 
   return (
-    <>
-    <h1>Andressa Isidoro</h1>
-
-    </>
-  )
+    <div>
+      <p>Você clicou {contador} vezes</p>
+      <button onClick={incrementar}>+</button>
+      <button onClick={decrementar}>-</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
