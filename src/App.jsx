76d91325ca5039bddc085cb79896434a90be0a62
Andressa Isidoro <dessa_silva93@outlook.com>
2024-05-28 21:43:01 -0300

    git pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [contador, setContador] = useState(0);
-
-  const incrementar = () => {
-    setContador(contador + 1);
-  };
-
-  const decrementar = () => {
-    setContador(contador - 1);
-  };
+    const nome = "Andressa";
+    const sobrenome = "Isidoro";
 
   return (
     <div>
-      <p>Você clicou {contador} vezes</p>
-      <button onClick={incrementar}>+</button>
-      <button onClick={decrementar}>-</button>
+      <p>{nome} {sobrenome}</p>
     </div>
   );
 }
